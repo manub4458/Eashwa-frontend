5260bb2e8e44f519c189d0a7c07c1364a48fac6c
Piyush Thakur <piyushthakur241199@gmail.com>
2026-03-05 23:28:28 +0530

    changes

--- app/pages/form.jsx
+++ app/pages/form.jsx
@@ -75,46 +75,15 @@ const BatteryForm = () => {
   };
 
   function getFormattedDate() {
-    const date = new Date();
-    const day = date.getDate();
-    const monthNames = [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December",
-    ];
-    const month = monthNames[date.getMonth()];
-    const year = date.getFullYear();
-    const hours = date.getHours();
-    const minutes = date.getMinutes();
-    const ampm = hours >= 12 ? "PM" : "AM";
+    const now = new Date();
 
-    const daySuffix = (day) => {
-      if (day > 3 && day < 21) return "th";
-      switch (day % 10) {
-        case 1:
-          return "st";
-        case 2:
-          return "nd";
-        case 3:
-          return "rd";
-        default:
-          return "th";
-      }
-    };
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2, "0");
+    const day = String(now.getDate()).padStart(2, "0");
+    const hours = String(now.getHours()).padStart(2, "0");
+    const minutes = String(now.getMinutes()).padStart(2, "0");
 
-    const formattedHour = hours % 12 || 12;
-    const formattedMinute = minutes < 10 ? "0" + minutes : minutes.toString();
-
-    return `${day}${daySuffix(day)} ${month} ${year}, ${formattedHour}:${formattedMinute} ${ampm}`;
+    return `${year}-${month}-${day} ${hours}:${minutes}`;
   }
 
   const onSubmit = async (data) => {
